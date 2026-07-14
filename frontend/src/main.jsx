4782062556262ba5aa8692e907870777eec11901
spectating101 <s1133958@mail.yzu.edu.tw>
2026-07-15 01:24:09 +0800

    Load V2 browser-review polish after shared shell styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,11 +5,12 @@ import { CaseWorkbenchProvider } from './app/CaseWorkbenchProvider';
 import './index.css';
 import './decisionBriefPolish.css';
 import './constraintProtocolHardening.css';
+import './styles/caseWorkbenchVisualPolish.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CaseWorkbenchProvider>
       <App />
     </CaseWorkbenchProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
