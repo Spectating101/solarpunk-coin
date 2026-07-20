3f274bacf751756f2f8c5a7d903d46414cb38ab2
spectating101 <s1133958@mail.yzu.edu.tw>
2026-07-20 18:01:59 +0800

    Load case investigation polish

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,6 +9,7 @@ import './styles/caseWorkbenchVisualPolish.css';
 import './styles/productPolish.css';
 import './styles/policyDisclosurePolish.css';
 import './styles/receiptPolish.css';
+import './styles/caseInvestigationPolish.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
