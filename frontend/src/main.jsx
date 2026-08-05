a383fc83801d2bd3c74d472e27c9873c8e91a19e
spectating101 <s1133958@mail.yzu.edu.tw>
2026-08-05 22:23:34 +0800

    fix(frontend): activate full-analysis guard at app root

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { CaseWorkbenchProvider } from './app/CaseWorkbenchProvider';
+import FullAnalysisRouteGuard from './app/FullAnalysisRouteGuard';
 import StudyProofNavigator from './components/StudyProofNavigator';
 import './index.css';
 import './decisionBriefPolish.css';
@@ -16,6 +17,7 @@ import './styles/caseInvestigationLayoutTuning.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CaseWorkbenchProvider>
+      <FullAnalysisRouteGuard />
       <App />
       <StudyProofNavigator />
     </CaseWorkbenchProvider>
