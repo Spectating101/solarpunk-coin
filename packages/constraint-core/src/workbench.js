de27fe946e4f9b16d68f6102863a08c56161c277
spectating101 <s1133958@mail.yzu.edu.tw>
2026-08-16 01:47:49 +0800

    feat(assessment): export derived assessment builder

--- packages/constraint-core/src/workbench.js
+++ packages/constraint-core/src/workbench.js
@@ -24,6 +24,7 @@ export * from './receipt.js';
 export * from './claim.js';
 export * from './decisionClaim.js';
 export * from './settlementConstraint.js';
+export * from './assessment.js';
 export * from './capsuleVerify.js';
 export * from './operatorIntake.js';
 
